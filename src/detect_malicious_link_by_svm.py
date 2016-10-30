f3bd946f2b8be6dd45e0766ec87fb4eb41a25efb
littleeleventhwolf <2367785195@qq.com>
2016-10-30 17:58:09 +0800

    import the required module

---
+++ src/detect_malicious_link_by_svm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import random
+
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizre
+from sklearn import svm
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
