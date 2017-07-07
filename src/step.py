8f82e588c13f617d7eae493df6e102b70ccaa2df
littleeleventhwolf <2367785195@qq.com>
2017-07-07 16:14:26 +0800

    sort the line

--- src/step.py
+++ src/step.py
@@ -1,28 +1,27 @@
-from pyspark.sql.types import StructType
-from pyspark.sql.types import StructField
+from pyspark.sql.types import StructType, StructField
 from pyspark.sql.types import IntegerType, StringType
-
-schema = StructType([StructField("url", StringType()), StructField("label", IntegerType())])
-
-df = spark.read.schema(schema).csv("file:///home/hadoop/Documents/SVM-on-Malicious-Link/data/datacp.csv", header=True)
-
-from pyspark.ml.feature import RegexTokenizer
-
-tokenizer = RegexTokenizer(inputCol="url", outputCol="words", pattern="/")
-
-from pyspark.ml.feature import HashingTF, IDF
-
-hashingTF = HashingTF(inputCol=tokenizer.getOutputCol(), outputCol="rawFeatures", numFeatures=20)
-
-idf = IDF(inputCol=hashingTF.getOutputCol(), outputCol="features")
-
+from pyspark.ml.feature import RegexTokenizer, HashingTF, 
 from pyspark.ml.classification import LogisticRegression
-
-lr = LogisticRegression(maxIter=100, regParam=0.001)
-
 from pyspark.ml import Pipeline
 
+# schema for dataframe
+schema = StructType([StructField("url", StringType()), StructField("label", IntegerType())])
+# read the data to generate a dataframe
+df = spark.read.schema(schema).csv("file:///home/hadoop/Documents/LR-on-Malicious-Link/data/datacp.csv", header=True)
+
+# string regex tokenizer
+tokenizer = RegexTokenizer(inputCol="url", outputCol="words", pattern="/")
+# hashing term frequency
+hashingTF = HashingTF(inputCol=tokenizer.getOutputCol(), outputCol="rawFeatures", numFeatures=20)
+# inverse document frequency
+idf = IDF(inputCol=hashingTF.getOutputCol(), outputCol="features")
+
+# logistic tregression
+lr = LogisticRegression(maxIter=100, regParam=0.001)
+
+# add stages to pipeline
 pipeline = Pipeline(stages=[tokenizer, hashingTF, idf, lr])
 
+# train the model
 model = pipeline.fit(df)
 
